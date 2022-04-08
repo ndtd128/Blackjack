2ac5eb22ba05b94f82cc1301ffb93f3e07a29201
ndtd128 <ndtd128@gmail.com>
2022-04-09 01:12:29 +0700

    update gameConstants.h

--- gameConstants.h
+++ gameConstants.h
@@ -4,3 +4,8 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 const std::string WINDOW_TITLE = "Blackjack";
+
+//Game starter pack
+const int PLAYER_START_CASH = 100;
+
+const int MAX_DECK_SIZE = 52;
