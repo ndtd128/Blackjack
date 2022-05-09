c5ee137f24fb1ffc8985da39677114deb83f85bd
ndtd128 <ndtd128@gmail.com>
2022-05-09 14:37:42 +0700

    add baseObject class

---
+++ BaseObject.cpp
@@ -0,0 +1,37 @@
+#include "BaseObject.h"
+
+BaseObject::BaseObject()
+{
+	object = nullptr;
+	frame = { 0,0,0,0 };
+}
+
+BaseObject::~BaseObject()
+{
+	if (object != nullptr) {
+		SDL_DestroyTexture(object);
+		object = nullptr;
+		frame = { 0,0,0,0 };
+	}
+}
+
+void BaseObject::loadTexture(SDL_Renderer* &ren, const std::string& filePath)
+{
+	object = IMG_LoadTexture(ren, filePath.c_str());
+	if (object == nullptr)
+		std::cout << "Unable to load texture. Error: " << SDL_GetError() << std::endl;
+	SDL_QueryTexture(object, NULL, NULL, &frame.w, &frame.h);
+}
+
+void BaseObject::render(SDL_Renderer* ren, const int& x, const int& y)
+{
+	frame.x = x;
+	frame.y = y;
+	SDL_RenderCopy(ren, object, NULL, &frame);
+}
+
+void BaseObject::render(SDL_Renderer* ren, const int& x, const int& y, const int& w, const int& h)
+{
+	frame = { x,y,w,h };
+	SDL_RenderCopy(ren, object, NULL, &frame);
+}
---
+++ BaseObject.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <string>
+
+
+
+class BaseObject
+{
+public:
+	BaseObject();
+	~BaseObject();
+
+	SDL_Texture* getTex() const { return object; }
+	SDL_Rect getFrame() const { return frame; }
+
+	void loadTexture(SDL_Renderer* &ren, const std::string& filePath);
+	void render(SDL_Renderer* ren, const int& x, const int& y);
+	void render(SDL_Renderer* ren, const int& x, const int& y, const int& w, const int& h);
+
+
+private:
+	SDL_Texture* object;
+	SDL_Rect frame;
+};
+
+static BaseObject table;
+static BaseObject cardObj[13][4];
+static BaseObject backCard;
