0184249872720e6d0367b9bd6e3e962a635006d4
ndtd128 <ndtd128@gmail.com>
2022-05-09 14:41:30 +0700

    update game constants

--- gameConstants.h
+++ gameConstants.h
@@ -1,11 +1,17 @@
 #pragma once
-#include <string>
+#include <SDL.h>
+
 //Window
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-const std::string WINDOW_TITLE = "Blackjack";
+const char WINDOW_TITLE[] = "Blackjack";
 
 //Game starter pack
 const int PLAYER_START_CASH = 100;
 
-const int MAX_DECK_SIZE = 52;
+const int FULL_DECK_SIZE = 52;
+const int MAX_PLAYER_CARDS = 12;
+const int CARD_WIDTH = 90;
+const int CARD_HEIGHT = CARD_WIDTH * 3 / 2;
+const int CARD_XSPACE = CARD_WIDTH / 3;
+const int CARD_YSPACE = CARD_HEIGHT / 2;
